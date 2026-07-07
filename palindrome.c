23ba8d5c4cb27275422c08001435ebcf895e7de0
Adebayo Sukurat Omolara <127861458+lollytech@users.noreply.github.com>
2026-07-07 20:40:33 +0100

    add palindrome

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int number, original, remainder, reverse = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    original = number;
+
+    while (number != 0) {
+        remainder = number % 10;
+        reverse = reverse * 10 + remainder;
+        number = number / 10;
+    }
+
+    if (original == reverse) {
+        printf("%d is a Palindrome.\n", original);
+    } else {
+        printf("%d is NOT a Palindrome.\n", original);
+    }
+
+    return 0;
+}
